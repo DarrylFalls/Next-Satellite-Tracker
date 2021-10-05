c4d68e8d07b723023c6740e618bd99fc8becd6ba
Darryl Falls <darryl.a.falls@gmial.com>
2021-10-04 20:39:54 -0400

    satellites and data showing

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const satFetch = (data) => {
 
 
 const addSatellites = (data) => {
-  const display = document.querySelector('#action-area')
+  removeOld()
   console.log(getDate(data.culmination))
   differentSatellites(data)
 }
@@ -50,7 +50,6 @@ const getTime12 = (data) => {
   let pre = null
   let hourNum = null
   let h = dt[11] + dt[12]
-  // let hInt = parseInt(h)
   if (h > 12) {
     if (h < 22) {
       hourNum = `0${h - 12}`
@@ -93,9 +92,9 @@ const differentSatellites = (data) => {
   const riseAlt = data.rise.alt
   const culmAlt = data.culmination.alt
   const setAlt = data.set.alt
-  const risePerc = (Math.floor((riseAlt / 90) * 10000) / 100)
-  const culmPerc = (Math.floor((culmAlt / 90) * 10000) / 100)
-  const setPerc = (Math.floor((setAlt / 90) * 10000) / 100)
+  const risePerc = (Math.floor((riseAlt / 90) * 10000) / 10000)
+  const culmPerc = (Math.floor((culmAlt / 90) * 10000) / 10000)
+  const setPerc = (Math.floor((setAlt / 90) * 10000) / 10000)
   const satArr = [
     {
       position: 'Rise',
@@ -129,18 +128,119 @@ const differentSatellites = (data) => {
     }
   ]
   console.log(satArr)
-  console.log(satArr[1].time)
   let timeOfDay = satArr[1].time24[0]+satArr[1].time24[1]
-  console.log(timeOfDay)
   setBackground(timeOfDay)
-  const risingSat = document.createElement('div')
-  const culmSat = document.createElement('div')
-  const settingSat = document.createElement('div')
+  buildSatellites(satArr)
+  
+  const compass = document.createElement('img')
+  document.querySelector('#compass-div').appendChild(compass)
+  compass.src = 'images/compass.png'
+  compass.style.height = '100px'
+  compass.style.width = '100px'
+  compass.style.position = 'center'
+  compass.className = 'compass'
+  compass.style.transform = `rotate(${360 - (satArr[1].az)}deg)`
+  console.log(satArr[1].az)
+
 }
 
 const buildSatellites = (arr) => {
+  let satOrder = getSatOrder(arr)
   arr.forEach(sat => {
+    const container = document.createElement('div')
+    if (sat.position == 'Culmination') {
+      document.querySelector('#action-center').appendChild(container)
+    } else if (sat.position == 'Rise' && satOrder == 123) {
+      document.querySelector('#action-left').appendChild(container)
+    } else if (sat.position == 'Rise' && satOrder == 321) {
+      document.querySelector('#action-right').appendChild(container)
+    } else if (sat.position == 'Set' && satOrder == 123) {
+      document.querySelector('#action-right').appendChild(container)
+    } else if (sat.position == 'Set' && satOrder == 321) {
+      document.querySelector('#action-left').appendChild(container)
+    }
     
+    container.className = 'satellite'
+    container.id = `${sat.position}`
+    container.style.height = '50px'
+    container.style.width = '50px'
+    // container.style.background = `url(images/satellite.png)`
+    // container.style.backgroundSize = 'contain'
+    // container.style.backgroundRepeat = 'no-repeat'
+    container.style.marginBottom = `${450 * sat.altPerc}px`
+    container.style.display = 'flex'
+    container.style.flexDirection = 'column'
+    container.style.alignItems = 'center'
+    
+    const img = document.createElement('img')
+    container.appendChild(img)
+    img.src = 'images/satellite.png'
+    img.style.width = '100%'
+    img.style.height = '80%'
+
+    
+
+    // smallInfoBox.style.transform = 'rotate(90deg)'
+
+    if (window.innerWidth > 600) {
+      const smallInfoBox = document.createElement('div')
+    container.appendChild(smallInfoBox)
+    smallInfoBox.style.background = 'rgba(200, 200, 200, 0.7)'
+    smallInfoBox.style.width = '200%'
+    smallInfoBox.style.textAlign = 'center'
+
+    const place = document.createElement('p')
+    smallInfoBox.appendChild(place)
+    place.innerText = `${sat.position}`
+    place.style.color = 'white'
+    place.style.marginBottom = '0'
+    place.style.marginTop = '0'
+
+    const time = document.createElement('p')
+    smallInfoBox.appendChild(time)
+    time.innerText = `${sat.time}`
+    time.style.color = 'white'
+    time.style.marginTop = '0'
+    time.style.marginBottom = '0'
+
+    const altitude = document.createElement('p')
+    smallInfoBox.appendChild(altitude)
+    altitude.innerText = `${sat.alt} degrees`
+    altitude.style.color = 'white'
+    altitude.style.marginTop = '0'
+    altitude.style.marginBottom = '0'
+    } else {
+      const smallInfoBox = document.createElement('div')
+      container.appendChild(smallInfoBox)
+      smallInfoBox.style.background = 'rgba(200, 200, 200, 0.7)'
+      smallInfoBox.style.width = '100%'
+      smallInfoBox.style.textAlign = 'center'
+
+      const place = document.createElement('p')
+      smallInfoBox.appendChild(place)
+      if (sat.position == 'Culmination') {
+        place.innerText = 'Apex'
+      } else {
+        place.innerText = `${sat.position}`
+      }
+      place.style.color = 'white'
+      place.style.marginBottom = '0'
+      place.style.marginTop = '0'
+
+      const time = document.createElement('p')
+    smallInfoBox.appendChild(time)
+    time.innerText = `${sat.time24[0]}${sat.time24[1]}${sat.time24[2]}${sat.time24[3]}${sat.time24[4]}`
+    time.style.color = 'white'
+    time.style.marginTop = '0'
+    time.style.marginBottom = '0'
+
+      const altitude = document.createElement('p')
+    smallInfoBox.appendChild(altitude)
+    altitude.innerText = `${sat.alt}°`
+    altitude.style.color = 'white'
+    altitude.style.marginTop = '0'
+    altitude.style.marginBottom = '0'
+    }
   });
 }
 
@@ -148,8 +248,44 @@ const setBackground = (time) => {
   const background = document.querySelector('#action-area')
   if (time >= 20 || time <= 5) {
     background.style.background = `url(images/night-sky.jpg)`
-  } else if (time < 20 && time > 5) {
+  } else if (time < 19 && time > 6) {
     background.style.background = 'rgb(70, 104, 160)'
+  } else if (time == 19) {
+    background.style.background = 'linear-gradient(to top, rgb(177, 58, 2), rgb(46, 2, 97))'
+  } else if (time == 6) {
+    background.style.background = 'linear-gradient(to top, rgb(197, 139, 62), rgb(45, 47, 160))'
   }
 }
 
+const getSatOrder = (arr) => {
+  let rise = arr[0].az
+  let culm = arr[1].az
+  let set = arr[2].az
+  if (rise < culm && culm < set) {
+    return 123
+  } else if (set < culm && culm < rise) {
+    return 321
+  } else if (rise > culm && culm < set && rise > set) {
+    return 123
+  } else if (set > culm && rise > culm && set > rise) {
+    return 321
+  } else if (rise < culm && rise > set && culm > set) {
+    return 123
+  } else if (set < culm && set > rise && culm > rise) {
+    return 321
+  }
+}
+
+const removeOld = () => {
+  const satellites = document.querySelectorAll('.satellite')
+  // if (satellites.length > 0) {
+  //   satellites.remove()
+  // }
+  satellites.forEach(satellite => {
+    satellite.remove()
+  })
+  const compass = document.querySelector('.compass')
+  if (compass !== null) {
+    compass.remove()
+  }
+}
